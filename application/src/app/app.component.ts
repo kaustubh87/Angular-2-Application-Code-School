f2bec7ba59ff67bc79962818ca08c84efdf55993
kvin1087 <kaustubh87@gmail.com>
2017-11-27 22:32:43 -0600

    Changed TemplateUrl to template

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+  template: `<h1>Ultra Racing</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
