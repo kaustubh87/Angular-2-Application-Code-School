a3716d20cb8c6786bb9a88f8ee69bea0344bacc1
kvin1087 <kaustubh87@gmail.com>
2017-11-30 08:58:17 -0600

    used ngFor loop to iterate objects of carParts array

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -3,15 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-selector',
   template: `<h1>{{ title }}</h1>
-  <h2>{{ carPart.name }}</h2>
-  <p>{{ carPart.description }}</p>
-  <p>{{ carPart.inStock }} in Stock</p>`,
+  <ul>
+    <li *ngFor="let carPart of carParts">
+    <h2>{{ carPart.name }}</h2>
+    <p>{{ carPart.description }}</p>
+    <p>{{ carPart.inStock }} in Stock</p>
+  </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Ultra Racing';
 
-  carPart = {
+  carParts = {
     'id': 1,
     'name' : 'Super Tires',
     'description': 'These tire sare very best',
