ae8eac6a26e271afab7d2e0f21bbde094ab7b0ec
kvin1087 <kaustubh87@gmail.com>
2017-11-29 23:51:00 -0600

    Added properties in the template

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-selector',
-  template: `<h1>{{ title }}</h1>`,
+  template: `<h1>{{ title }}</h1>
+  <h2>{{ carPart.name }}</h2>
+  <p>{{ carPart.description }}</p>
+  <p>{{ carPart.inStock }} in Stock</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
