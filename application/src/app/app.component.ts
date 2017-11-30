73a09a2990342c003bf5b9a3c22f713a5f3bcfdd
kvin1087 <kaustubh87@gmail.com>
2017-11-30 09:28:09 -0600

    Added objects in carPart

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -15,10 +15,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Ultra Racing';
 
-  carParts = {
+  carParts = [{
     'id': 1,
     'name' : 'Super Tires',
     'description': 'These tire sare very best',
     'inStock' : 5
-  };
+  },
+  {
+  'id': 2,
+  'name' : 'Reinforced Tires',
+  'description': 'Shocks made fro mkryptonite',
+  'inStock' : 4
+  },
+  {
+  'id' : 3,
+  'name' : 'Padded Seats',
+  'description': 'Super soft seats for smooth ride',
+  'inStock' : 0
+
+
+  }];
 }
