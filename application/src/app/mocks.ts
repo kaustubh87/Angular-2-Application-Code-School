2189df84d15ad11d44f0fa0d5d4fd1bd77b3bf97
kvin1087 <kaustubh87@gmail.com>
2017-12-05 00:58:02 -0600

    Fake Data

---
+++ application/src/app/mocks.ts
@@ -0,0 +1,23 @@
+import { CarPart } from './car-part';
+
+export const CARPARTS: CarPart[] = [{
+    'id': 1,
+    'name' : 'Super Tires',
+    'description': 'These tires are very best',
+    'inStock' : 5,
+    'price' : 4.99
+  },
+  {
+  'id': 2,
+  'name' : 'Reinforced Tires',
+  'description': 'Shocks made fro mkryptonite',
+  'inStock' : 4,
+  'price': 9.99
+  },
+  {
+  'id' : 3,
+  'name' : 'Padded Seats',
+  'description': 'Super soft seats for smooth ride',
+  'inStock' : 0,
+  'price': 10.99
+  }];
