48bfbf8b5d27602a12b1029140c6ba88e333e1e6
kvin1087 <kaustubh87@gmail.com>
2017-12-06 21:39:14 -0600

    Updated image path

--- application/src/app/mocks.ts
+++ application/src/app/mocks.ts
@@ -5,19 +5,22 @@ export const CARPARTS: CarPart[] = [{
     'name' : 'Super Tires',
     'description': 'These tires are very best',
     'inStock' : 5,
-    'price' : 4.99
+    'price' : 4.99,
+    'image' : '../app/images/car-parts-1.png'
   },
   {
   'id': 2,
   'name' : 'Reinforced Tires',
   'description': 'Shocks made fro mkryptonite',
   'inStock' : 4,
-  'price': 9.99
+  'price': 9.99,
+  'image' : '../app/images/car-parts-2.png'
   },
   {
   'id' : 3,
   'name' : 'Padded Seats',
   'description': 'Super soft seats for smooth ride',
   'inStock' : 0,
-  'price': 10.99
+  'price': 10.99,
+  'image': '../app/images/car-parts-3.png'
   }];
